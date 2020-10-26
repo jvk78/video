86889f9e8fd37fe122dba1fd18761bf871630dbc
jvk <60311804+jvk78@users.noreply.github.com>
2020-10-26 06:06:17 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const play = document.getElementById('play');
 const video = document.getElementById('video');
 video.volume = 0.1
+//play.onclick = function(){
+//  if (video.paused){
+//    video.play()
+//  } else {
+//    play.textContent = '*Play*'
+//    video.pause()
+//  }
+//}
+
 play.onclick = function(){
-  if (video.paused){
-    video.play()
-    play.textContent = '*Pause*'
-  } else {
-    play.textContent = '*Play*'
-    video.pause()
-  }
+    play.textContent += '*Pause*'
+  
 }
