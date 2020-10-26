2edf175b13b3ebf462fdce697ef89557a139bb2b
jvk <60311804+jvk78@users.noreply.github.com>
2020-10-26 06:00:15 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const play = document.getElementById('play');
 const video = document.getElementById('video');
 video.volume = 0.1
-play.addEventListener('click', function(){
+play.onclick = function(){
   if (video.paused){
     video.play()
-    play.textContent = 'Pause'
+    play.textContent = '*Pause*'
   } else {
-    play.textContent = 'Play'
+    play.textContent = '*Play*'
     video.pause()
   }
-})
+}
