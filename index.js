83595b2f9758138e9dc073a1c9f75176bf58b0c4
jvk <60311804+jvk78@users.noreply.github.com>
2020-10-27 15:49:43 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -2,14 +2,62 @@
 
 var play = document.getElementById('play');
 var video = document.getElementById('video');
-video.volume = 0.1;
-play.textContent = 'Go';
-play.addEventListener('click', function () {
-  if (video.paused) {
-    play.textContent = '*Play*';
-    video.play();
-  } else {
-    play.textContent = '*Play*';
-    video.pause();
-  }
-});
+var delay = void 0;
+var update = void 0;
+var updateDelay = void 0;
+var noActiveDelay = 5;
+var nowNoActiv = 0;
+//let resetnowNoActiv;
+video.volume = 0;
+play.onclick = toggleFullScreen;
+
+function toggleFullScreen() {
+    if (video.requestFullScreen) {
+        video.requestFullScreen();
+    } else if (video.webkitRequestFullScreen) {
+        video.webkitRequestFullScreen();
+    } else if (video.mozRequestFullScreen) {
+        video.mozRequestFullScreen();
+    } else if (video.msRequestFullscreen) {
+        video.msRequestFullscreen();
+    } else if (video.webkitEnterFullscreen) {
+        video.webkitEnterFullscreen(); //for iphone this code worked
+    }
+}
+//window.onload = () => {
+// console.log(video.onloadedmetadata)
+//  if (video.paused){
+//hideTools()
+//    video.play()
+//  } else {
+//    play.textContent = '*Play*'
+//    video.pause()
+//  }
+//} 
+//
+//function hideTools() {
+//
+//delay = setInterval('nowNoActiv++', 1000)
+//update = setInterval('updateDelay()', 1000)
+//  
+//
+//function resetnowNoActiv() {nowNoActiv = 0}
+//updateDelay = function() {
+//  
+////  console.log(nowNoActiv)
+//  if (nowNoActiv >= noActiveDelay) {
+//    clearInterval(delay)
+//    clearInterval(update)
+//    cons()
+//    return;
+//  }
+//}
+//
+//  window.onclick = resetnowNoActiv
+//  window.onmousemove = resetnowNoActiv
+//  window.ontouchstart = resetnowNoActiv
+//  window.ontouchmove = resetnowNoActiv
+//
+//}
+////const cons = () => console.log('Menu hide')
+//}
